df8d3a0ce1d02a00178ca6994efb2e9535a6dd73
zhongsheng <nily.zhong@gmail.com>
2019-04-01 23:31:02 +0800

    add Array method

--- index.js
+++ index.js
@@ -5,3 +5,37 @@ String.prototype.to_i = function(){
 Number.prototype.to_s = function(){
     return this.toString()
 }
+
+Array.prototype.fetch = function(point, opt=''){
+    return this[point] || opt
+}
+
+Array.prototype.size = function(){
+    return this.length
+}
+
+Array.prototype.select = function(func){
+    let ary = []
+    this.forEach(x => {
+        if(func(x)){
+            ary.push(x)
+        }
+    })
+    return ary
+}
+
+global.even = function(arg){
+    if(arg%2 == 0){
+        return true
+    }else{
+        return false
+    }
+}
+
+global.odd = function(arg){
+    if(arg%2 == 0){
+        return false
+    }else{
+        return true
+    }
+}
--- test/index.test.js
+++ test/index.test.js
@@ -2,11 +2,23 @@ require('../index.js')
 var expect = require('chai').expect
 
 describe('index test', function(){
-    it('should return integer', function(){
+    var ary = [1,2,3,5,7]
+    it('String.to_i', function(){
         expect( '1'.to_i() ).to.be.equal(1)
     })
-    it('should return string', function(){
+    it('Number.to_s', function(){
         var number = 123
         expect( number.to_s() ).to.be.equal('123')
+    })
+    it('Array.fetch method',function(){
+        expect(ary.fetch(9, 'zero')).to.be.equal('zero')
+        expect(ary.fetch(9, 0)).to.be.equal(0)
+        expect(ary.fetch(2)).to.be.equal(3)
+    })
+    it('Array have select method', function(){
+        expect(ary.select(even)[0] ).to.be.equal(2)
+    })
+    it('Array have odd and size method', function(){
+        expect(ary.select(odd).size() ).to.be.equal(4)
     })
 })
