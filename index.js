7c4abf8a4f8406d33c6b29cf7108a74f70e58f77
zhongsheng <nily.zhong@gmail.com>
2019-04-02 14:46:51 +0800

    add global function

--- index.js
+++ index.js
@@ -6,14 +6,50 @@ Number.prototype.to_s = function(){
     return this.toString()
 }
 
+Number.prototype.even = function(){
+    if(this%2){
+        return false
+    }else{
+        return true
+    }
+}
+
 Array.prototype.fetch = function(point, opt=''){
     return this[point] || opt
+}
+Array.prototype.count = function(){
+    return this.length
 }
 
 Array.prototype.size = function(){
     return this.length
 }
 
+Array.prototype.reject = function(func){
+    let ary = []
+    this.forEach(x => {
+        if(!func(x)){
+            ary.push(x)
+        }
+    })
+    return ary
+}
+
+Array.prototype.sum = function(func){
+    let sum_value = 0
+    this.forEach(x=>{
+        if(typeof func == 'function'){
+            if(func(x) === true){
+                sum_value += x
+            }else{
+                sum_value += func(x)
+            }
+        }else{
+            sum_value += x
+        }
+    })
+    return sum_value
+}
 Array.prototype.select = function(func){
     let ary = []
     this.forEach(x => {
@@ -23,8 +59,30 @@ Array.prototype.select = function(func){
     })
     return ary
 }
+Array.prototype.chunk = function(func){
+    let ary = []
+    let items = []
+    this.forEach(x=>{
+        var state = func(x)
+        if(items[0] === state){
+            items[1].push(x)
+        }else{
+            if(items.length > 0){
+                ary.push(items)
+                items = []
+            }
+            items.push(state,[])
+            items[1].push(x)
+        }
+    })
+    ary.push(items)
+    return ary
+}
 
-global.even = function(arg){
+global.even = function(arg,arg_c){
+    if(typeof arg_c == 'number' ){
+       return arg%2 - arg_c%2
+    }
     if(arg%2 == 0){
         return true
     }else{
@@ -34,8 +92,12 @@ global.even = function(arg){
 
 global.odd = function(arg){
     if(arg%2 == 0){
-        return false
+        return 0
     }else{
-        return true
+        return arg
     }
+}
+
+global.pp = function(arg){
+    console.log(arg)
 }
--- test/index.test.js
+++ test/index.test.js
@@ -3,6 +3,8 @@ var expect = require('chai').expect
 
 describe('index test', function(){
     var ary = [1,2,3,5,7]
+    var obj = {name: 'Jon', phone: '123'}
+    var num = 1
     it('String.to_i', function(){
         expect( '1'.to_i() ).to.be.equal(1)
     })
@@ -10,6 +12,11 @@ describe('index test', function(){
         var number = 123
         expect( number.to_s() ).to.be.equal('123')
     })
+
+    it('Number.even', function(){
+        expect(num.even()).to.be.equal(false)
+    })
+
     it('Array.fetch method',function(){
         expect(ary.fetch(9, 'zero')).to.be.equal('zero')
         expect(ary.fetch(9, 0)).to.be.equal(0)
@@ -20,5 +27,28 @@ describe('index test', function(){
     })
     it('Array have odd and size method', function(){
         expect(ary.select(odd).size() ).to.be.equal(4)
+        expect(ary.select(odd).count() ).to.be.equal(4)
     })
+    it('Array reject method', function(){
+        expect(ary.reject(even).size()).to.be.equal(4)
+        expect(ary.reject(x => { return x > 3}).size()).to.be.equal(3)
+    })
+
+    it('Array sum',function(){
+        expect(ary.sum()).to.be.equal(18)
+        expect(ary.sum(even)).to.be.equal(2)
+        expect(ary.sum(odd)).to.be.equal(16)
+
+    })
+    it('Object select',function(){
+        expect(obj.select()).to.be.equal('1')
+    })
+
+    it('Array chunk',function(){
+        let ary = [3, 1, 4, 1, 5, 9, 2, 6, 5, 3, 5];
+        pp(ary.chunk(even))
+        expect( ary.chunk(even).size() ).to.be.equal(5)
+        expect( ary.sort(even).chunk(even).size() ).to.be.equal(2)
+    })
+
 })
